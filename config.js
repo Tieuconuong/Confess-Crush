4f145e7cef06ef69c29c4a0b62df7137fa126ec8
Tieuconuong <93829709+Tieuconuong@users.noreply.github.com>
2021-11-06 21:57:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,14 @@ const CONFIG = {
     Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    title: 'Cậu thích tớ lắm đúng hông 🥰',
+    btnYes: ' Thích lắm lun :33',
+    btnNo: 'Không nha',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Còn chần chờ j nữa mak ko ib vs mik',
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Còn chần chờ j nữa mak ko ib vs tớ',
     btnAccept: 'Okiiiii lun <3',
     messLink: '
 https://www.facebook.com/profile.php?id=100071862262639'
