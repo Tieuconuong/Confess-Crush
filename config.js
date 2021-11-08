eb2e227ce18a38ccfee30aa0dc91756c7a882f16
Tieuconuong <93829709+Tieuconuong@users.noreply.github.com>
2021-11-08 08:45:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,11 @@
 const CONFIG = {
     titleWeb: Gửi crush
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao anh cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
     title: 'Cậu thích tớ lắm đúng hông 🥰',
     btnYes: ' Thích lắm lun :33',
     btnNo: 'Không nha',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao cậu lại thích tớ z <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Vì cậu dễ huông,đáng iu <33333333',
     mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
     messDesc: 'Còn chần chờ j nữa mak ko ib vs tớ',
     btnAccept: 'Okiiiii lun <3',
